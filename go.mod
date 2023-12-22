b529091f906b4be973930a27e578d8bfff75219b
JohnGolgota <js684new@gmail.com>
2023-12-22 19:16:58 +0000

    commit inicial se me termino el almuerzo ;-;

---
+++ go.mod
@@ -0,0 +1,3 @@
+module example.com/web-service-gin
+
+go 1.21.4
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+func getAlbums(c *gin.Context)  {
+	c.
+}
